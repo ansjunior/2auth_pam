920acc7c8b31a19f3fba47c080923a4a0e633af8
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-07 00:24:20 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -1,11 +1,3 @@
-#include <dirent.h>
-#include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <security/pam_modules.h>
 #include <security/pam_ext.h>
 
