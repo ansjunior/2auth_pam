e87198a0b8a737b67a3a8cbbb9186e7296771804
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-07 00:11:44 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -62,6 +62,87 @@ login_root(){
 		
 }
 
+login_user(){
+	    struct tm *data_hora_atual;
+		time_t segundos;    
+		time(&segundos);   
+		data_hora_atual = localtime(&segundos);  
+
+		ssize_t len;
+		 
+		char buf[256], *p;
+		char buf2[256];
+		int i;
+		
+		 len = readlink("/sys/block/sdb", buf, 256);
+	   
+		buf[len] = 0;
+	   
+		// printf("%s\n", buf);
+		sprintf(buf2, "%s/%s", "/sys/block/", buf);
+		for (i=0; i<6; i++) {
+			p = strrchr(buf2, '/');
+			*p = 0;
+		}
+	   
+		// printf("%s\n", buf2);
+		strcat(buf2, "/serial");
+		// printf("opening %s\n", buf2);
+
+		int f = open(buf2, 0);
+		len = read(f, buf, 256);
+		if (len <= 0) {
+			
+		}
+	 
+		buf[len - 1 ] = 0;
+		
+		FILE *g = fopen("/etc/pam.d/pam.pdrive/pdrive.serial", "r");
+		size_t serial = 100; 
+		char *linha = malloc(serial);
+		int estado;
+		
+		if (!g)
+		  {
+			perror("/etc/pam.d/pam.pdrive/pdrive.serial");
+			exit(1);
+		  }
+		  while (getline(&linha, &serial, g) > 0)
+		  {
+			if(strcmp(buf, linha) == 0){
+				system("clear");
+			
+				FILE *log;
+
+				log = fopen("/etc/pam.d/pam.pdrive/log/login", "a");
+				fprintf(log, "%s", "-> Serial: ");
+				fprintf(log, "%s | ", buf);
+				fprintf(log, "%d/", data_hora_atual->tm_mday);
+				fprintf(log, "%d/", data_hora_atual->tm_mon+1);
+				fprintf(log, "%d ", data_hora_atual->tm_year+1900);
+				fprintf(log, "%d:", data_hora_atual->tm_hour);
+				fprintf(log, "%d:", data_hora_atual->tm_min);
+				fprintf(log, "%d\n ", data_hora_atual->tm_sec);
+
+			  printf("\n\n  -> Pendrive Liberado!");
+			  printf("\n          -> Logando..\n\n");
+			  
+			  return PAM_SUCCESS;
+
+			}else{
+				system("clear");
+				printf("\n\n  -> Pendrive bloqueado!");
+				printf("\n          -> Insira um pendrive liberado e tente novamente.\n\n");
+				  
+				return PAM_AUTH_ERR;
+			}
+		  }
+		  if (linha){
+			free(linha);
+		  }
+		  fclose(g);
+}
+
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) 
 {
