99b8ee2b0b0a2ee9c2465fc9482af3a114ae89b3
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-07 00:21:39 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -6,12 +6,6 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <time.h>
-#include <errno.h>
-#include <pwd.h>
-#include <security/pam_modules.h>
-#include <security/pam_ext.h>
-#include <zconf.h>
 
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) 
