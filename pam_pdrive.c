4fc7add90d7d9df3f3f5c9bc3a404ce6915d86ca
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-07 20:51:17 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -17,6 +17,7 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 		if(r == "lsblk: /dev/sdb: not a block device")
 	{
 		printf("aqui mesmo");
+		return PAM_SUCCESS;
 
 	}	else{
 	
