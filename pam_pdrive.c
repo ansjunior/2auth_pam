a15e3ccf3cb6f74bec2f6fb490afa66b7e39822f
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-07 21:23:43 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -1,6 +1,7 @@
 #include <security/pam_modules.h>
 #include <security/pam_ext.h>
-#include <stdlib.h>
+#include <stdlib.h>]
+#include <stdio.h>
 
 
 PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) 
@@ -14,7 +15,7 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 	int r;
 	r=system("lsblk --output SERIAL /dev/sdb");
 	return r; 
-	printf("aqui mesmo");
+	printf(r);
 	return PAM_SUCCESS;
 
 	
