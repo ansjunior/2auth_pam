c69014f4833706dd5b9beec45b79144b50c5cfd9
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-07 21:20:49 -0300

    Update criar_arquivos.c

--- criar_arquivos.c
+++ criar_arquivos.c
@@ -18,7 +18,7 @@ void makeDirPdrive(){
 	
     //se existe, retorna msg, se não cria o diretório
 	if (stat("/etc/pam.d/pam.pdrive", &st) == -1) {
-		if(mkdir("/etc/pam.d/pam.pdrive", 0700) == 0){
+		(mkdir("/etc/pam.d/pam.pdrive", 0700) == 0){
             printf("    Diretório criado com Sucesso: /pam.pdrive\n");
 		}else{
             printf("    Erro ao criar diretório: /pam.pdrive\n");
@@ -30,139 +30,6 @@ void makeDirPdrive(){
 
 
 
-void makeFileSerial(){
-    FILE *pdrive_serial;
-    
-    pdrive_serial = fopen("/etc/pam.d/pam.pdrive/pdrive.serial", "a");
-
-    if (pdrive_serial == NULL){
-        printf("    Erro ao criar arquivo de seriais.\n");
-    }
-    else{
-        printf("    Arquivo de seriais criado com sucesso.\n");
-    }
-}
-
-int verPendrive(){
-    ssize_t len;
-     
-    char buf[256], *p;
-    char buf2[256];
-    int i;
-   
-    len = readlink("/sys/block/sdb", buf, 256);
-   
-    buf[len] = 0;
-   
-    // printf("%s\n", buf);
-    sprintf(buf2, "%s/%s", "/sys/block/", buf);
-    for (i=0; i<6; i++) {
-        p = strrchr(buf2, '/');
-        *p = 0;
-    }
-   
-    // printf("%s\n", buf2);
-    strcat(buf2, "/serial");
-    // printf("opening %s\n", buf2);
-
-    int f = open(buf2, 0);
-    len = read(f, buf, 256);
-    if (len <= 0) {
-        printf("Erro");
-    }
- 
-    buf[len - 1] = 0;
-
-    int qtd = strlen(buf);
-
-    return qtd;
-    
-}
-
-char verSerial(){
-    ssize_t len;
-     
-    char buf[256], *p;
-    char buf2[256];
-    int i;
-   
-    len = readlink("/sys/block/sdb", buf, 256);
-   
-    buf[len] = 0;
-   
-    // printf("%s\n", buf);
-    sprintf(buf2, "%s/%s", "/sys/block/", buf);
-    for (i=0; i<6; i++) {
-        p = strrchr(buf2, '/');
-        *p = 0;
-    }
-   
-    // printf("%s\n", buf2);
-    strcat(buf2, "/serial");
-    // printf("opening %s\n", buf2);
-
-    int f = open(buf2, 0);
-    len = read(f, buf, 256);
-    if (len <= 0) {
-        
-    }
- 
-    buf[len - 1] = 0;
-    
-
-    return buf;
-
-}
-
-
-void addUser( char nome[256] ){
-    ssize_t len;
-     
-    char buf[256], *p;
-    char buf2[256];
-    int i;
-   
-    len = readlink("/sys/block/sdb", buf, 256);
-   
-    buf[len] = 0;
-   
-    sprintf(buf2, "%s/%s", "/sys/block/", buf);
-    for (i=0; i<6; i++) {
-        p = strrchr(buf2, '/');
-        *p = 0;
-    }
-   
-    strcat(buf2, "/serial");
-
-    int f = open(buf2, 0);
-    len = read(f, buf, 256);
-    if (len <= 0) {
-        
-    }
- 
-    buf[len - 1] = 0;
-
-    FILE *pont_arq; // cria variável ponteiro para o arquivo
-        
-        //abrindo o arquivo com tipo de abertura w
-        pont_arq = fopen("/etc/pam.d/pam.pdrive/pdrive.serial", "w");
-        
-        //testando se o arquivo foi realmente criado
-        if(pont_arq == NULL)
-        {
-        printf("    Error!");
-        }
-        
-        //usando fprintf para armazenar a string no arquivo
-        fprintf(pont_arq, "%s", buf);
-
-        //usando fclose para fechar o arquivo
-        fclose(pont_arq);
-
-        puts("    Work done successfully\n");
-
-}
-
 int main(int argc, char *argv[]){
     system("clear");
 
@@ -172,7 +39,7 @@ int main(int argc, char *argv[]){
     system("clear"); 
 
      puts("\n-> Installing:");
-    puts("<><><><><><");
+    puts("<><><><><><>");
     sleep(2);
 
     puts("\n-> Creating directories:");
@@ -180,16 +47,6 @@ int main(int argc, char *argv[]){
      makeDirPdrive();
      sleep(2);
 
-    puts("\n-> Creating files to save serials:");
-    puts("   '/etc/pam.d/pam.pdrive'");
-    makeFileSerial();
-    
-
-    char nome[100]; 
-
-    puts("\n-> Allowing pendrive:");
-
-    addUser(nome);
 
     system("read -p \"Press enter to continue\" foo");
 
