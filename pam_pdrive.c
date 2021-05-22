1b524184e3f34c31c5fe94f86fd654361dfbd1ae
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-22 18:00:50 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -11,12 +11,16 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
 {
-	
-	if(strcmp(system("cat /etc/pam.d/pam.pdrive/teste.txt"), system("cat /etc/pam.d/pam.pdrive/teste.txt"))== 0)
+	char r;
+	char a;
+	r = system("cat /etc/pam.d/pam.pdrive/teste.txt");
+	a = system("cat /etc/pam.d/pam.pdrive/teste.txt");
+
+	if(system("cat /etc/pam.d/pam.pdrive/teste.txt") == system("cat /etc/pam.d/pam.pdrive/teste.txt")))
 	{
-				printf("seriais iguais");
-				sleep(2);
-				return PAM_SUCCESS;
+		printf("seriais iguais");
+		sleep(2);
+		return PAM_SUCCESS;
 	}
 	else 
 	{
