88f1c611cf7cd4cff562a5079004f161bdd9562d
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-24 21:15:40 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -4,10 +4,7 @@
 #include <stdio.h>
 
 
-PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) 
-{
-	return PAM_SUCCESS;
-}
+
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
 {
