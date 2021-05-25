f66e374b3c2e8f987a93bf6cedbb4aa51479661e
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-05-24 22:28:11 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -13,17 +13,18 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 {
 	
 
-	if(strcmp(system("lsblk --output SERIAL /dev/sdb"),system("cat /etc/pam.d/pam.pdrive/teste.txt")) == 0)
+	if(system("lsblk --output SERIAL /dev/sdb") != system("cat /etc/pam.d/pam.pdrive/teste.txt"))
+	{
+		printf("\nSeriais diferentes\n");
+		sleep(2);
+		return PAM_AUTH_ERR;		
+
+	}
+	else 
 	{
 		printf("\nseriais iguais\n");
 		sleep(2);
 		return PAM_SUCCESS;
-	}
-	else 
-	{
-		printf("\nSeriais diferentes\n");
-		sleep(2);
-		return PAM_AUTH_ERR;
 	}
 
 	
