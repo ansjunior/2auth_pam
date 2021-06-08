10b6f960b270f5c57e8f16cb272c3f890d7fc179
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-06-07 22:15:38 -0300

    Update pdrive.c

--- pdrive.c
+++ pdrive.c
@@ -16,31 +16,31 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
 {
 	
-	system("/sbin/udevadm info -n /dev/sdb -a | grep serial > /etc/pam.d/pam.pdrive/teste2.txt");
+	system("/sbin/udevadm info -n /dev/sdb -a | grep serial > /etc/pam.d/pam.pdrive/pen-drive2.txt");
 	char pdrive[512];
 	char pdrive2[512];
 
 	FILE *pendrive1;
 	FILE *pendrive2;
 		
-	pendrive1 = fopen("/etc/pam.d/pam.pdrive/teste2.txt", "r");
-	pendrive2 = fopen("/etc/pam.d/pam.pdrive/teste.txt", "r");
+	pendrive1 = fopen("/etc/pam.d/pam.pdrive/pen-drive2.txt", "r");
+	pendrive2 = fopen("/etc/pam.d/pam.pdrive/pen-drive.txt", "r");
 
 	fgets(pdrive, 512, pendrive1);
 	fgets(pdrive2, 512, pendrive2);
 	
-		sleep(2);
+	sleep(1);
 		
 	if(strcmp(pdrive, pdrive2) == 0)
 	{
-		printf("\nseriais iguais\n");
-		sleep(2);
+		printf("\nOs seriais são iguais\n");
+		sleep(1);
 		return PAM_SUCCESS;
 	}
 	else 
 	{
-		printf("\nSeriais diferentes\n");
-		sleep(2);
+		printf("\nOs seriais são diferentes\n");
+		sleep(1);
 		return PAM_AUTH_ERR;		
 
 	}
