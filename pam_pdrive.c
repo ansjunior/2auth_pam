f7ad2d1ddf2652e3e41a1a61d72ddf459ab8b4e7
Anisio Junior <47663717+ansjunior@users.noreply.github.com>
2021-06-07 21:51:32 -0300

    Update pam_pdrive.c

--- pam_pdrive.c
+++ pam_pdrive.c
@@ -15,23 +15,7 @@ PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const ch
 
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) 
 {
-	/*int serial1, serial2, result;
 	
-	FILE *pendrive1;
-	FILE *pendrive2;
-	pendrive1 = popen("lsblk --output SERIAL /dev/sdb", "r");
-	pendrive2 = popen("cat /etc/pam.d/pam.pdrive/teste.txt", "r");
-	
-	
-	serial1 = pclose(pendrive1);
-	serial2 = pclose(pendrive2);
-	
-	char a[60];
-	char b[60];
-
-	fgets(a, 512 , serial1);
-	fgets(b, 512 , serial2);*/
-//system("lsblk --output SERIAL /dev/sdb > /etc/pam.d/pam.pdrive/teste2.txt");
 	system("/sbin/udevadm info -n /dev/sdb -a | grep serial > /etc/pam.d/pam.pdrive/teste2.txt");
 	char pdrive[512];
 	char pdrive2[512];
@@ -45,20 +29,9 @@ PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, cons
 	fgets(pdrive, 512, pendrive1);
 	fgets(pdrive2, 512, pendrive2);
 	
-	if(strcmp(pdrive, pdrive2) == 0)
-	   {	
-		printf("iguais");
-	}
-	else {
-		printf("nao iguais");
-	}
-	
-	printf("Esse é um %s", &pdrive);
-	
-	
-	sleep(2);
+		sleep(2);
 	fclose(pendrive1);
-	if(system("lsblk --output SERIAL /dev/sdb") == system("cat /etc/pam.d/pam.pdrive/teste.txt"))
+	if(strcmp(pdrive, pdrive2) == 0))
 	{
 		printf("\nseriais iguais\n");
 		sleep(2);
